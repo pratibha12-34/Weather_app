af188614c00d189bfba36959d7a8da9d37a38081
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-06-23 09:26:31 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from "react";
+import Sidebar from "./assets/components/Sidebar";
+import Header from "./assets/components/Header";
+import OverviewChart from "./assets/components/OverviewChart";
+import AppointmentList from "./assets/components/AppointmentLis";
+import PatientList from "./assets/components/PatientList";
+import './App.css'
+
+function App() {
+  return (
+    <div className="dashboard-container">
+      <Sidebar />
+      <div className="main-content">
+        <Header />
+      <div className="top-section">
+        <OverviewChart />
+        <AppointmentList />
+      </div>
+        <div className="bottom-section">
+          <PatientList />
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
